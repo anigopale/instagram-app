4769e3890e10e0ab23270c3cb038b7c21855aee1
Aniruddh Gopale <anigopale@gmail.com>
2017-12-29 09:50:26 +0530

    create tags component

--- src/components/iapp.js
+++ src/components/iapp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Search from './search_bar';
+import Tags from './tags';
 import { TAGS_URL } from '../assets/urls';
 
 export default class Iapp extends Component {
@@ -37,6 +38,7 @@ export default class Iapp extends Component {
       return (
         <div>
           <Search onSearchSubmit = {this.onSearchSubmit.bind(this)} />
+          <Tags />
         </div>
       )
     }
